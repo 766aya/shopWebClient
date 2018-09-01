57fd7f2bb5cb4aafc6d6e1cc7474b9711b424a9f
766aya <1494233259@qq.com>
2018-09-01 11:50:03 +0800

    update

--- config/index.js
+++ config/index.js
@@ -12,11 +12,11 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         "/goods": {
-            target: "http://212.64.17.113:3000",
+            target: "http://127.0.0.1:3000",
             changeOrigin:true,
         },
         "/users": {
-            target: "http://212.64.17.113:3000",
+            target: "http://127.0.0.1:3000",
             changeOrigin:true,
         }
         // "/goods": {
